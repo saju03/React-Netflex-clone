86abb11f89f1d1735c26da7cf623001481f2b4bc
saju03 <sajusuresh03@gmail.com>
2023-01-16 09:52:22 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyARSyhshDPWB3xnCw7De-67lrjZ8FaQyak",
+  authDomain: "netflex-clone-43b67.firebaseapp.com",
+  projectId: "netflex-clone-43b67",
+  storageBucket: "netflex-clone-43b67.appspot.com",
+  messagingSenderId: "369864499909",
+  appId: "1:369864499909:web:6a59d2913b501c51b446ac",
+  measurementId: "G-Q5B2YDE6S7"
+};
